ee3afffbea64cd958eb12ce13a10670c856c3e80
Mitch Dzugan <mitchdzugan@gmail.com>
2026-01-05 22:45:13 -0600

    pyside6-project new-quick slp-sync

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+from pathlib import Path
+
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtCore import QUrl
+from PySide6.QtQml import QQmlApplicationEngine
+
+
+if __name__ == "__main__":
+    app = QGuiApplication()
+    engine = QQmlApplicationEngine()
+    qml_file = Path(__file__).parent / 'main.qml'
+    engine.load(QUrl.fromLocalFile(qml_file))
+    if not engine.rootObjects():
+        sys.exit(-1)
+    exit_code = app.exec()
+    del engine
+    sys.exit(exit_code)
